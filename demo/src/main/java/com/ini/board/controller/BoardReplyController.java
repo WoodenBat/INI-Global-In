d5c49eb2372f54297bf006af27da9bdcfedaa932
WoodenBat <145077555+WoodenBat@users.noreply.github.com>
2025-06-18 18:22:06 +0900

    댓글 작업중(50%)

--- demo/src/main/java/com/ini/board/controller/BoardReplyController.java
+++ demo/src/main/java/com/ini/board/controller/BoardReplyController.java
@@ -1,11 +1,17 @@
 package com.ini.board.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ini.board.service.BoardReplyService;
+import com.ini.board.vo.BoardReplyDTO;
 
 import lombok.AllArgsConstructor;
 
@@ -15,19 +21,26 @@ import lombok.AllArgsConstructor;
 public class BoardReplyController {
 
 	private BoardReplyService boardReplyService;
-	
+
 	@GetMapping("boardReply")
-	public String boardReplyList(Model model, String board_id) {
-		
-		model.addAttribute("boardReplyList", boardReplyService.findBoardReplyByBoardId(board_id));
-		
-		return "board/boardReply";
+	@ResponseBody
+	public List<BoardReplyDTO> boardReplyList(@RequestParam("board_id") String board_id) {
+
+		return boardReplyService.findBoardReplyByBoardId(board_id);
 	}
-	
+
 	@GetMapping("boardReplyTest")
 	public String boardReplyTest() {
-		
+
 		return "board/boardReplyTest";
 	}
-	
+
+	@PostMapping("boardReplyInsert")
+	@ResponseBody
+	public void boardReplyInsert(@RequestParam("board_id") String board_id,
+			@RequestParam("reply_content") String reply_content, @RequestParam("reply_writer") String reply_writer) {
+		
+		
+	}
+
 }
