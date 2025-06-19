b074e42363729ed91957835f93cda223bf6ce8bb
kdg5616 <kdg2309847@gmail.com>
2025-06-20 02:12:55 +0900

    Add files via upload

---
+++ demo/src/main/java/com/ini/board/vo/BoardTagVO.java
@@ -0,0 +1,10 @@
+package com.ini.board.vo;
+
+import lombok.Data;
+
+@Data
+public class BoardTagVO {
+    private String boardTag;      // BOARD_TAG
+    private String boardTagJp;    // BOARD_TAG_JP
+}
+
