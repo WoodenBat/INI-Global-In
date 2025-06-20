ee2c6da9087fccf82ec47e99a7b5717183a32aa9
WoodenBat <145077555+WoodenBat@users.noreply.github.com>
2025-06-20 09:53:53 +0900

    정현 민철 머지

--- demo/src/main/java/com/ini/board/mapper/BoardMapper.java
+++ demo/src/main/java/com/ini/board/mapper/BoardMapper.java
@@ -3,20 +3,39 @@ package com.ini.board.mapper;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import com.ini.board.vo.BoardCategoryVO;
 import com.ini.board.vo.BoardDTO;
+import com.ini.board.vo.BoardReplyDTO;
+import com.ini.board.vo.BoardTagVO;
 
 @Mapper
 public interface BoardMapper {
 
-    List<BoardDTO> selectBoardList(
-        @Param("keyword") String keyword,
-        @Param("category") String category,
-        @Param("startRow") int startRow,
-        @Param("pageSize") int pageSize
-    );
+	List<BoardDTO> selectBoardList(@Param("keyword") String keyword, @Param("category") String category,
+			@Param("startRow") int startRow, @Param("pageSize") int pageSize);
 
-    int countBoardList(
-        @Param("keyword") String keyword,
-        @Param("category") String category
-    );
+	int countBoardList(@Param("keyword") String keyword, @Param("category") String category);
+
+	// 한국어용
+	List<BoardCategoryVO> getAllBoardCategoriesKr();
+
+	List<BoardTagVO> getAllBoardTagsKr();
+
+	// 일본어용
+	List<BoardCategoryVO> getAllBoardCategoriesJp();
+
+	List<BoardTagVO> getAllBoardTagsJp();
+
+	// 작성게시글저장
+	void insertBoard(BoardDTO board);
+
+	// ID에맞춰 게시글가져오기
+	BoardDTO getBoardById(int board_id);
+
+	// 댓글 가져오기
+	List<BoardReplyDTO> getRepliesByBoardId(int board_id);
+
+	// 댓글 저장하기
+	void insertReply(BoardReplyDTO reply);
 }
--- demo/src/main/java/com/ini/board/service/BoardService.java
+++ demo/src/main/java/com/ini/board/service/BoardService.java
@@ -12,32 +12,28 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class BoardService {
-	
-<<<<<<< HEAD
-	 private final BoardMapper boardMapper;
+
+	private final BoardMapper boardMapper;
 
 	public List<BoardDTO> getBoardList(String keyword, String category, int page, int pageSize) {
-	    int startRow = (page - 1) * pageSize;
-	    return boardMapper.selectBoardList(keyword, category, startRow, pageSize);
+		int startRow = (page - 1) * pageSize;
+		return boardMapper.selectBoardList(keyword, category, startRow, pageSize);
 	}
 
 	public int getBoardCount(String keyword, String category) {
-	    return boardMapper.countBoardList(keyword, category);
+		return boardMapper.countBoardList(keyword, category);
 	}
-}
-=======
-	
+
 	public int saveBoard(BoardDTO board) {
-		boardmapper.insertBoard(board);
-		
+		boardMapper.insertBoard(board);
+
 		return board.getBoard_id();
 	}
-	
+
 	public BoardDTO getBoardById(int board_id) {
-		
-		BoardDTO board = boardmapper.getBoardById(board_id);
-		
+
+		BoardDTO board = boardMapper.getBoardById(board_id);
+
 		return board;
 	}
 }
->>>>>>> refs/remotes/origin/mincheol
